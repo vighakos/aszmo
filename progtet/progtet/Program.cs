8236e405a765a2ead021b1c8de3eb48902777153
vighakos <vighakos@turr.hu>
2022-09-05 10:33:54 +0200

    .

--- progtet/progtet/Program.cs
+++ progtet/progtet/Program.cs
@@ -8,17 +8,48 @@ namespace progtet
 {
     class Program
     {
-        static List<int> elemek = new List<int>() { 23, 45, 67, 32, 21, 24, 576, 45, 986, 12, 1, 54, 765, 43, 59, 120, 516, 89, 142, 32, 321, 654, 97, 156, 971, 111, 222, 555, 666, 999, 0, 74, 61 };
+        static List<int> elemek = new List<int>() { 23, 45, 67, 32, 21, 24, 576, 45, 986, 12, 1, 54, 765, 43, 59, 120, 516, 89, 1, 142, 32, 321, 654, 97, 156, 971, 1, 111, 222, 555, 666, 999, 0, 74, 61, 1, 1, };
         static void Main(string[] args)
         {
             MaxKivalaszt(); 
             MinKivalaszt();
             Osszeg();
             Atlag();
-            NovekRendez();
-            CsokkenRendez();
+            //NovekRendez();
+            //CsokkenRendez();
+            AdottElem();
 
             Console.ReadKey();
+        }
+
+        private static void AdottElem()
+        {
+            Console.Write("Adj meg egy számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            List<int> helyek = new List<int>();
+            bool van = true;
+            int db = 0;
+
+            for (int i = 0; i < elemek.Count; i++)
+            {
+                if (elemek[i] == szam)
+                {
+                    helyek.Add(i);
+                    van = false;
+                    db++;
+                }
+            }
+
+            if (van) Console.WriteLine("Nincs");
+            else
+            {
+                Console.Write($"Van, {db}x, a következő helyeken: ");
+                foreach (int item in helyek)
+                {
+                    Console.Write($"{item + 1}, ");
+                }
+            }
+                
         }
 
         private static void CsokkenRendez()
---
+++ progtet2/progtet2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progtet2
+{
+    class Program
+    {
+        static List<int> a = new List<int>() { 34, 34, 56, 4, 87, 15, 99, 52, 36, 48, 21, 4, 98, 11, 11, 10, 0, 78, 65, 59, 58, 68, 41, 32, 74};
+        static List<int> b = new List<int>() { 34, 34, 55, 6, 4, 97, 15, 99, 5, 36, 48, 0, 88, 31, 4, 98, 11, 63, 1, 10, 0, 78, 65, 58, 41, 32, 74, 57, 66};
+        static void Main(string[] args)
+        {
+            Unio();
+            Metszet();
+            Kulonbseg();
+
+            Console.ReadKey();
+        }
+
+        private static void Kulonbseg()
+        {
+            List<int> kulonbseg = new List<int>();
+
+            foreach (int item in a)
+            {
+                if (Tartalmaz(a, item) && !Tartalmaz(b, item))
+                {
+                    kulonbseg.Add(item);
+                }
+            }
+
+            Console.WriteLine("\n\naa küoldönség:");
+            foreach (int item in kulonbseg)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+
+        private static void Metszet()
+        {
+            List<int> ab = new List<int>();
+
+            foreach (int item in a)
+            {
+                foreach (int item2 in b)
+                {
+                    if (item == item2 && !Tartalmaz(ab, item))
+                    {
+                        ab.Add(item);
+                    }
+                }
+            }
+
+            Console.WriteLine("\n\naZ másik izében az ememeklemek:");
+            foreach (int item in ab)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+
+        private static void Unio()
+        {
+            List<int> ab = new List<int>();
+
+            foreach (int item in a)
+            {
+                foreach (int item2 in b)
+                {
+                    if (!Tartalmaz(ab, item))
+                    {
+                        ab.Add(item);
+                    }
+                }
+            }
+
+            Console.WriteLine("aZ izében az ememeklemek:");
+            foreach (int item in ab)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+
+        private static bool Tartalmaz(List<int> list, int item)
+        {
+            foreach (int item2 in list)
+            {
+                if (item == item2) return true;
+            }
+            return false;
+        }
+    }
+}
